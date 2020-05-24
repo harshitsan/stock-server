aaba4c5189b4eaadf23d038e0c55ab7e9a54e527
harshitsan <harshitbhalla.hb@gmail.com>
2020-05-24 21:55:39 +0530

    update apis

--- routes/stocks.js
+++ routes/stocks.js
@@ -3,85 +3,111 @@ var router = express.Router({ caseSensitive: true });
 const krex = require("knex")({
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    user: "abhinav",
-    password: "password",
+    host: "localhost",
+    user: "newuser",
+    password: "abc123abc",
     database: "webcomputing",
   },
 });
 
 router.get("/symbols", function (req, res, next) {
-  var response = [];
-  const industry = req.query.industry;
-    if( !industry ){
-        if ( Object.keys(req.query).length > 0 ){
-            res.status(400).send({ error : true, message : "Invalid query parameter: only 'industry is permitted'" })
-        }
+  try {
+    const { industry } = req.query;
+    if (!industry && Object.keys(req.query).length > 0) {
+      return res.status(400).send({
+        error: true,
+        message: "Invalid query parameter: only 'industry is permitted'",
+      });
+    } else {
+      if (industry) {
+        console.log(industry);
+        krex
+          .select("*")
+          .from("stocks")
+          .where("industry", industry)
+          .then((rows) => {
+            if (rows.length)
+              return res.status(200).send(JSON.parse(JSON.stringify(rows)));
+            else
+              return res
+                .status(404)
+                .send({ error: true, message: "Industry selector not found" });
+          })
+          .catch((e) => {
+            console.log(e);
+            return res
+              .status(500)
+              .send({ error: true, message: "Server Error" });
+          });
+      } else
+        krex
+          .select("name", "symbol", "industry")
+          .from("stocks")
+          .distinct()
+          .then((rows) => {
+            res.status(200).send(JSON.parse(JSON.stringify(rows)));
+          })
+          .catch((e) => {
+            console.log(e);
+            return res
+              .status(500)
+              .send({ error: true, message: "Server Error" });
+          });
     }
-  if(industry){
-      krex
-        .select("*")
-        .from("stocks")
-        .where("industry", industry)
-          .then( rows => {
-              if( rows.length == 0 )
-                  res.send(404).send({ error : true, message : "Industry selector not found" })
-          } ).catch();
-      krex
-        .select("name", "symbol", "industry")
-        .from("stocks")
-        .distinct()
-        .where("industry", industry)
-        .then((rows) => {
-            res.status(200).send(JSON.parse(JSON.stringify(rows)))
-        })
-        .catch();
-  }else{
-      krex
-        .select("symbol")
-        .from("stocks")
-        .distinct()
-        .then((rows) => res.status(200).send(JSON.parse(JSON.stringify(rows))))
-        .catch();
-    }
-})
+  } catch (e) {
+    return res.status(500).send({ error: true, message: "Server Error" });
+  }
+});
 
 router.get("/:symbol([A-Z]+)", function (req, res, next) {
-    if( Object.keys(req.query).length > 0 ){
-        res.send({ error : true, message : "Data parameters only available on authenticated route /stocks/authed" })
+  try {
+    if (Object.keys(req.query).length > 0) {
+      res.status(400).send({
+        error: true,
+        message:
+          "Date parameters only available on authenticated route /stocks/authed",
+      });
     }
-  krex
-    .select("*")
-    .from("stocks")
-    .where({ symbol: req.params.symbol })
-    .orderBy("timestamp")
-    .first()
-        .then((row) => {
-            if( !row ){
-                res.send(404).send({ error : true, message : "No entry for symbol in stocks database" });
-            }
-            res.send(JSON.parse(JSON.stringify(row)))
-        })
-    .catch((err) => res.send(err));
+    krex
+      .select("*")
+      .from("stocks")
+      .where({ symbol: req.params.symbol })
+      .orderBy("timestamp")
+      .first()
+      .then((row) => {
+        if (!row) {
+          res.send(404).send({
+            error: true,
+            message: "No entry for symbol in stocks database",
+          });
+        }
+        res.status(200).send(JSON.parse(JSON.stringify(row)));
+      })
+      .catch((err) => res.send(err));
+  } catch (e) {
+    return res.status(500).send({ error: true, message: "Server Error" });
+  }
 });
 
 router.get("/authed/:symbol([A-Z]+)", function (req, res, next) {
   jwt.verify(req.header["user-token"], SECRET_KEY, (err, decoded) => {
-      if (err){
-          res.status(403).send({ error : true, message : "Authorization header not found" });
-      }
+    if (err) {
+      res
+        .status(403)
+        .send({ error: true, message: "Authorization header not found" });
+    }
     if (!err) {
       krex
         .select("*")
         .from("stocks")
         .where({ symbol: req.params.symbol })
         .orderBy("timestamp")
-            .then((rows) => {
-                if ( rows.lenght > 0 ){
-                    res.send(JSON.parse(JSON.stringify(rows)))
-                }
-                res.status(404).send()
-            })
+        .then((rows) => {
+          if (rows.lenght > 0) {
+            res.send(JSON.parse(JSON.stringify(rows)));
+          }
+          res.status(404).send();
+        })
         .catch((err) => res.send(err));
     }
   });
--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,10 @@ var router = express.Router();
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    user: "abhinav",
-    password: "password",
-    database: "users",
+    host: "localhost",
+    user: "newuser",
+    password: "abc123abc",
+    database: "webcomputing",
   },
 });
 
@@ -19,24 +19,45 @@ router.post(
   function (req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ error : true, message : "Request body imcomplete - email and password needed" });
+      return res.status(400).json({
+        error: true,
+        message: "Request body incomplete - email and password needed",
+      });
     }
-    knex
-      .select("email")
-      .from("users")
-      .where({ email: req.body.email })
-      .then((rows) => {
-        if (rows.length > 0) {
-          return res.send({ error: true, message: "User already exists!" });
-        }
-      });
     try {
-      bcrypt.hash(req.body.password, 10, (err, hashvalue) => {
-        knex
-          .insert({ email: req.body.email, password: hashvalue })
-          .into("users");
-      });
-      return res.status(201).send({ success: true, message: "User created" });
+      knex
+        .select("email")
+        .from("users")
+        .where({ email: req.body.email })
+        .then((rows) => {
+          console.log(rows);
+          if (rows.length > 0) {
+            return res.send({ error: true, message: "User already exists!" });
+          }
+
+          bcrypt.hash(req.body.password, 10, (err, hashvalue) => {
+            console.log(hashvalue);
+            knex
+              .insert({ email: req.body.email, password: hashvalue })
+              .into("users")
+              .then((result) => {
+                console.log(result, "rrrrrrr");
+                return res
+                  .status(201)
+                  .send({ success: true, message: "User created" });
+              })
+              .catch((e) => {
+                console.log(e);
+                return res
+                  .status(500)
+                  .send({ error: true, message: "Server Error" });
+              });
+          });
+        })
+        .catch((e) => {
+          console.log(e);
+          return res.status(500).send({ error: true, message: "Server Error" });
+        });
     } catch (exp) {
       return res.status(500).send({ error: true, message: "Server Error" });
     }
@@ -44,33 +65,34 @@ router.post(
 );
 
 router.post("/login", function (req, res, next) {
-    knex
-      .select("email", "password")
-      .from("users")
-      .where({ email: req.body.email })
-      .first()
-      .then((row) => {
-          if(!row){
-              res.send(401).send({ error : true, message : "Incorrect password" })
-          }
-        if (bcrpyt.compareSync(req.body.password, row["password"])) {
-          jwt.sign(
-            { email: req.body.password },
-            privatKey,
-            { expiresIn: "24h" },
-              (token) => res.status(200).send({
-                  token : token,
-                  token_type : "Bearer",
-                  expires : 86400
-              })
-          );
-        }else {
-              res.send(401).send({ error : true, message : "Incorrect password" })
-        }
-      })
-      .catch((err) => {
-        res.send("error");
-      });
+  knex
+    .select("email", "password")
+    .from("users")
+    .where({ email: req.body.email })
+    .first()
+    .then((row) => {
+      if (!row) {
+        res.send(401).send({ error: true, message: "Incorrect password" });
+      }
+      if (bcrpyt.compareSync(req.body.password, row["password"])) {
+        jwt.sign(
+          { email: req.body.password },
+          privatKey,
+          { expiresIn: "24h" },
+          (token) =>
+            res.status(200).send({
+              token: token,
+              token_type: "Bearer",
+              expires: 86400,
+            })
+        );
+      } else {
+        res.send(401).send({ error: true, message: "Incorrect password" });
+      }
+    })
+    .catch((err) => {
+      res.send("error");
+    });
 });
 
 module.exports = router;
